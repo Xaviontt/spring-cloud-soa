7f2ea79b70b7be1b23b501c6e581d74135b8ec27
Xaviontt <xavion@list.ru>
2024-02-06 20:15:27 +0300

    init

--- eureka-client/src/main/java/ru/soa/spring/cloud/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/ru/soa/spring/cloud/eurekaclient/controller/TestController.java
@@ -1,9 +1,7 @@
 package ru.soa.spring.cloud.eurekaclient.controller;
 
-import jakarta.ws.rs.GET;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.GetExchange;
 
 @RestController
 public class TestController {
